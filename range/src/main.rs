a3c4d358527e3dfe5fca24295ab31fce34a3e1cd
Исхаков Рустем Радикович <rriskhako1@mts.ru>
2022-08-31 00:15:50 +0300

    range

---
+++ range/src/main.rs
@@ -0,0 +1,18 @@
+fn first_word(s: &String) -> usize {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+
+    s.len()
+}
+
+fn main() {
+    let mut s = String::from("Hello, world!");
+
+    println!("{}", first_word(&s));
+}
+
